1352092db7053bb8ec36305b712d97f13ac88c5c
Jose Benito Figueroa Romero <jbfigueroa2000@gmail.com>
2022-08-30 10:09:06 -0500

    Creando un archivo index.js para organizar mejor el proyecto

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function agregarCliente(){
     htmlModal = document.getElementById("modal");
     htmlModal.setAttribute("class", "modale opened");
 }
+
 function cerrarModal(){
     htmlModal = document.getElementById("modal");
     htmlModal.setAttribute("class", "modale");
